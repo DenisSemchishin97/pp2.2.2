97b28d0c8cc774fef91723d480fe87aab33dd17a
DenisSemchishin97 <120304966+DenisSemchishin97@users.noreply.github.com>
2022-12-20 14:34:19 +0300

    Update CarService.java

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -5,7 +5,7 @@ import web.model.Car;
 import java.util.List;
 
 public interface CarService {
- public static List<Car> getCars(List<Car> list, int a) {
-  return getCars(list,a);
+ public static List<Car> getCars(int a) {
+  return getCars(a);
  }
 }
