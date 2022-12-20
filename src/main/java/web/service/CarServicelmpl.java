ad81232746138c4329643a032470e95f3f9c6a7c
DenisSemchishin97 <120304966+DenisSemchishin97@users.noreply.github.com>
2022-12-20 14:33:59 +0300

    Update CarServicelmpl.java

--- src/main/java/web/service/CarServicelmpl.java
+++ src/main/java/web/service/CarServicelmpl.java
@@ -1,13 +1,26 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Service
 public class CarServicelmpl implements CarService {
-
-    public static List<Car> getCars(List<Car> list, int a) {
-        return list.stream().limit(a).collect(Collectors.toList());
+    public static List<Car> getCars(int a) {
+        List<Car>cars=new ArrayList<>();
+        Car car1 = new Car("bmw", 3, "black");
+        Car car2 = new Car("mersedess", 202, "red");
+        Car car3 = new Car("vaz", 3, "white");
+        Car car4 = new Car("hyondai", 1, "yellow");
+        Car car5 = new Car("mazda", 6, "blue");
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+        return cars.stream().limit(a).collect(Collectors.toList());
     }
 }
+
