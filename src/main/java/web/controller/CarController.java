d6759f550b4e8a44343db2245303d20f2ed830f8
DenisSemchishin97 <120304966+DenisSemchishin97@users.noreply.github.com>
2022-12-22 17:05:15 +0300

    Update CarController.java

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,18 +1,21 @@
 package web.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarServicelmpl;
-import java.util.List;
 
 @Controller
 public class CarController {
+    private CarServicelmpl carService;
+    @Autowired
+    CarController(CarServicelmpl carService){
+        this.carService=carService;
+    }
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int a, Model model) {
-        List<Car> cars = CarServicelmpl.getCars(a);
-        model.addAttribute("cars", cars);
+        model.addAttribute("cars", carService.getCars(a));
         return "cars";
     }
 }
